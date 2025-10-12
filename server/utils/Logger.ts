21288298a22ef9661c9a9482491b5a72dc3c4d6d
Ivan Petrovskiy <jelbelvidere@gmail.com>
2025-10-12 22:39:22 +0300

    fixed logger

--- server/utils/Logger.ts
+++ server/utils/Logger.ts
@@ -1,13 +1,28 @@
-import fs from 'node:fs'
+import fs from "node:fs";
 
-export function log(level: 'info' | 'warn' | 'error', message: string, error?: Error | unknown): void {
-    const messageLog = `${new Date(Date.now()).toISOString()} --> ${message} ${error ? `--> ErrorLog: ${error}` : ''}`
-    fs.writeFileSync('./novabroker-logs.log', `${messageLog}\n`)
+const LOG_PATH = "./novabroker-logs.log";
 
-    if (level === 'info') console.info(messageLog)
-    if (level === 'warn') console.warn(messageLog)
-    if (level === 'error') {
-        mailErrorToAdmin(error, messageLog)
-        console.error(messageLog)
-    }
+export function log(
+  level: "info" | "warn" | "error",
+  message: string,
+  error?: Error | unknown
+): void {
+  const messageLog = `[${level}] ${new Date(Date.now()).toISOString()} --> ${message} ${
+    error ? `--> ErrorLog: ${error}` : ""
+  }`;
+
+  if (!fs.existsSync(LOG_PATH)) {
+    fs.writeFileSync(LOG_PATH, `Initial log\n`);
+  }
+
+  fs.appendFile(LOG_PATH, `${messageLog}\n`, function (err) {
+    if (err) throw err;
+  });
+
+  if (level === "info") console.info(messageLog);
+  if (level === "warn") console.warn(messageLog);
+  if (level === "error") {
+    mailErrorToAdmin(error, messageLog);
+    console.error(messageLog);
+  }
 }
